4f494a5148d57d04d0c47eda87a0989b70c16f3b
Naveen Jangid <135016113+cloudwifi@users.noreply.github.com>
2025-02-02 00:48:02 +0530

    Update remote_backend_s3.tf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-12345666666"
+    bucket = "dev-proj-1-remote-state-bucket12345666666"
     key    = "devops-project-1/terraform.tfstate"
     region = "eu-west-1"
   }
